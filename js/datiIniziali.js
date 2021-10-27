046c416b7f28f3369b02fee3d45eab5e16d8b634
ricardo-limaco <ricardolimaco.05@gmail.com>
2021-10-27 13:12:59 +0200

    JS

--- js/datiIniziali.js
+++ js/datiIniziali.js
@@ -1,4 +1,4 @@
-const arrayAnimal = [
+const icons = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -97,3 +97,77 @@ const arrayAnimal = [
 	}
 ];
 
+
+const iconsContainer = document.getElementById('icons-container');
+const selectCategory = document.getElementById('select-category');
+
+
+const animalIcons = icons.filter(icon => icon.type === 'animal');
+const vegetableIcons = icons.filter(icon => icon.type === 'vegetable');
+const userIcons = icons.filter(icon => icon.type === 'user');
+
+
+icons.forEach(element => createCard(element));
+
+let iconList = document.querySelectorAll('.col .card i');
+iconColour(iconList);
+
+
+
+
+selectCategory.addEventListener('change', function () {
+
+	iconsContainer.innerHTML = ''
+
+	switch (selectCategory.value) {
+		case 'all':
+			icons.forEach(element => createCard(element));
+			break;
+		case 'animal':
+			animalIcons.forEach(element => createCard(element));
+			break;
+		case 'vegetable':
+			vegetableIcons.forEach(element => createCard(element));
+			break;
+		case 'user':
+			userIcons.forEach(element => createCard(element));
+			break;
+	};
+
+	iconList = document.querySelectorAll('.col .card i');
+	iconColour(iconList)
+});
+
+
+
+function iconColour(iconsArray) {
+	iconsArray.forEach(element => {
+		switch (element.getAttribute('type')) {
+			case 'animal':
+				element.style.color = '#0d6efd';
+				break;
+			case 'vegetable':
+				element.style.color = '#198754';
+				break;
+			case 'user':
+				element.style.color = '#6610f2';
+				break;
+		};
+	});
+};
+
+
+
+function createCard(element) {
+
+	iconsContainer.innerHTML += `<div class="col">
+	<div class="card shadow-sm">
+		<div class="card-img-top text-center" style="font-size: 50px;">
+			<i class="${element.family} ${element.prefix}${element.name}" type="${element.type}"></i>
+		</div>
+		<div class="card-body">
+			<div class="card-title text-center text-capitalize fw-bold m-0">${element.name.split("-").join(" ")}</div>
+		</div>
+	</div>
+</div>`;
+};
